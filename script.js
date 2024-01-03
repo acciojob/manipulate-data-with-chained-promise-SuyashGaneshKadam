d2c5af1fa1acfe4224ab8a14726abe141b5b418a
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2024-01-03 21:16:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+const input = [1, 2, 3, 4];
+
+function promiseGenerator() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(input);
+    }, 3000);
+  });
+}
+
+promiseGenerator()
+  .then(arr => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        const filtered = arr.filter(num => num % 2 === 0);
+        document.getElementById("output").textContent = JSON.stringify(filtered);
+        resolve(filtered);  
+      }, 1000);
+    });
+  })
+  .then(arr => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        const multiplied = arr.map(num => num * 2);
+        document.getElementById("output").textContent = JSON.stringify(multiplied);
+        resolve(multiplied);
+      }, 2000);
+    });
+  });
